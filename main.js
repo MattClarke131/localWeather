027dc4717c82f89280c9d6fcabd4fdd34c0ce518
Matthew Clarke <mattclarkeruns@gmail.com>
2017-07-13 12:56:19 -0400

    Create convertTemp function

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ Written by Matthew Clarke
 function setTemp(newTemp) {
   document.getElementById('temperature').innerHTML = newTemp;
 };
+
+function convertTemp(oldTemp, newScale) {
+  if (newScale == "celsius") {
+    var newTemp = (5/9)*(oldTemp-32);
+  }
+  if (newScale == "farenheit") {
+    var newTemp = (9/5)*oldTemp+32;
+  }
+  return newTemp;
+}
