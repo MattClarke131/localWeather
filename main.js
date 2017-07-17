fd49d1bf419898b4422e1f001366d9963908d30d
Matthew Clarke <mattclarkeruns@gmail.com>
2017-07-17 13:38:12 -0400

    Create CORS request to FCC API
    CORS is preferred to JSON-P. JSON-P has some security issues.
    FCC API is broken and will always return the same result.

--- main.js
+++ main.js
@@ -10,12 +10,17 @@ if (navigator.geolocation) {
   console.log("Geolocation is not supported for this browser")
 };
 
+// Variables
 var latitude;
 var longitude;
+var weatherInfo;
+
 window.onload = function() {
   var geoSuccess = function(position) {
     latitude = position.coords.latitude;
+    latitude = latitude.toString();
     longitude = position.coords.longitude;
+    longitude = longitude.toString();
   };
   var geoError = function(error) {
     console.log("Error occured: " + error.code);
@@ -25,11 +30,50 @@ window.onload = function() {
     enableHighAccuracy: false,
   };
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+  xhr.send();
 }
 
 function setTemp(newTemp) {
   document.getElementById('temperature').innerHTML = newTemp;
 };
+
+// Cross-Origin Resource Sharing
+// https://www.html5rocks.com/en/tutorials/cors/
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if("withCredentials" in xhr) {
+    // Check if XMLHttpRequest object has "withCredentials" property.
+    // Chrome, Firefox, Opera, and Safari use XMLHttpRequest
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE uses XDomainRequest
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS is not supported by the browser
+    xhr = null;
+  }
+  return xhr;
+}
+
+var xhr = createCORSRequest('GET', 'https://fcc-weather-api.glitch.me/api/current?lat='+latitude+'&lon='+longitude);
+if (!xhr) {
+  throw new Error('CORS not supported');
+}
+xhr.onload = function() {
+  var responseText = xhr.responseText;
+  weatherInfo = JSON.parse(responseText);
+  updateWeatherDisplay();
+};
+xhr.onerror = function() {
+  console.log("Error in CORS Request!");
+};
+
+function updateWeatherDisplay() {
+  document.getElementById('temperature').innerHTML = weatherInfo.main.temp
+};
+
+
 
 function convertTemp(oldTemp, newScale) {
   if (newScale == "celsius") {
